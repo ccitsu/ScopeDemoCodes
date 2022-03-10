a3a3492e41297ce4c87b699cdb4a127b35c74c1b
Shaqra University <ccitlms@gmail.com>
2022-03-10 10:30:56 +0300

    Integrated Examples from Keynotes and Session!

--- ScopeDemo.playground/Contents.swift
+++ ScopeDemo.playground/Contents.swift
@@ -70,3 +70,70 @@ func myFunc () -> Int
 
 var retVal = myFunc()
 print(retVal)
+
+
+
+// EXAMPLES FROM KEYNOTES :
+
+var age = 55
+
+func printMyAge() {
+  print("My age: \(age)")
+}
+
+print(age)
+printMyAge()
+
+
+
+func printBottleCount() {
+    let bottleCount = 99
+    print(bottleCount)
+}
+
+printBottleCount()
+//print(bottleCount)
+//Errorenous Code!
+
+
+func printTenNames() {
+  let name = "Richard"
+  for index in 1...10 {
+    print("\(index): \(name)")
+  }
+  print(index)
+  print(name)
+}
+
+printTenNames()
+
+
+let points = 100
+
+for index in 1...3 {
+  let points = 200
+  print("Loop \(index): \(points+index)")
+}
+print(points)
+
+
+
+var newName: String? = "Robert"
+
+if let newName = newName {
+ print("My name is \(newName)")
+}
+
+func exclaim(name: String?) {
+  if let name = name {
+    print("Exclaim function was passed: \(name)")
+  }
+}
+
+func newExclaim(name: String?) {
+  guard let name = name else { return }
+  print("Exclaim function was passed: \(name)")
+}
+
+
+
